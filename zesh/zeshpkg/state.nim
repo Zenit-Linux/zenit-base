5efa2730eee5cbd16fd396c90fe29e34209980d4
michal92299 <chudybyczek2137@gmail.com>
2026-09-17 18:54:43 +0200

    Update state.nim

--- zesh/zeshpkg/state.nim
+++ zesh/zeshpkg/state.nim
@@ -5,3 +5,5 @@ var
   aliases*:     Table[string, string] = initTable[string, string]()
   history*:     seq[string] = @[]
   lastExitCode*: int = 0
+  scriptName*:  string = "zesh"  ## `$0` -- nazwa programu/skryptu (patrz zesh.nim: `-c` i tryb skryptowy)
+  scriptArgs*:  seq[string] = @[] ## `$1`..`$9`/`$@`/`$#` -- argumenty przekazane do `zesh SKRYPT arg1 arg2 ...`
